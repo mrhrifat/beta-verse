d421e25a378bd41425213c2f27f7c20ce12499b2
mrhrifat <mrhrifat383759@gmail.com>
2023-02-16 23:59:07 +0600

    ci: all components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,31 @@
 // import Image from "next/image";
+import { Footer, Navbar } from '@/components'
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from '@/sections'
 import { metadata as Metadata } from '@/utils/data'
 export const metadata = Metadata
-export default function Home() {
-  console.log('A')
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+export default function Home() {
+  return (
+    <div className="bg-primary-black overflow-hidden">
+      <Navbar />
+      <Hero />
+      <About />
+      <Explore />
+      <GetStarted />
+      <WhatsNew />
+      <World />
+      <Insights />
+      <Feedback />
+      <Footer />
+    </div>
+  )
 }
