1cbb953a8cd5a41ef07cb8f1f8f81399028f4b15
mrhrifat <mrhrifat383759@gmail.com>
2023-02-18 23:31:29 +0600

    build: metadata

--- utils/data.ts
+++ utils/data.ts
@@ -1,4 +1,24 @@
 const metadata = {
-  title: "Beta Verse",
-};
-export { metadata };
+  title: 'Beta Verse',
+  charset: 'UTF-8',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  description:
+    'Beta Verse is a mordern technology website built with NextJS 13, TypeScript, Eslint & Prettier configertation.',
+  keywords: [
+    'Beta Verse',
+    'mordern',
+    'technology',
+    'TypeScript',
+    'Eslint',
+    'Prettier',
+    'NextJS 13',
+  ],
+  icons: {
+    icon: '/favicon.ico',
+  },
+}
+export { metadata }
