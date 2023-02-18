da75a43160eac51a9bee6dc39069a2e2eecee4ad
mrhrifat <mrhrifat383759@gmail.com>
2023-02-18 23:30:19 +0600

    ci: extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,17 @@ module.exports = {
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx}',
   ],
+  mod: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#C7C7C7',
+      },
+      transitionTimingFunction: {
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
+      },
+    },
   },
   plugins: [],
 }
